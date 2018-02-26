b2004cad7a5864fd911bb65ba695a838f9db1c51
vzim <veronique@programmiertier.de>
2018-02-26 16:12:30 +0100

    for und foreach in php

--- text.php
+++ text.php
@@ -25,13 +25,37 @@ print "Zweite Zahl ist $zahl2 <br>";
 print "DER Satz in der Programmierung = " .$text1.$text2."<br>";
 
 //-----
+print "<hr/>";
+if($zahl2 == 3)
+{
+	print "Die Variable zahl2 hat den Wert 3<br/>";
+}
 
+while($zahl2 > 0)
+{
+	print $zahl2."<br/>";
+	$zahl2--;
+}
+
+do
+{
+	print $zahl2."<br/>";
+	$zahl2++;
+}
+while($zahl2 < 5);
+
+for( $i = 0; $i < 10; $i++)
+{
+	print "Immer noch Montag <br/>";
+}
+
+//-----
 $umsatz[0] = 1234;
 $umsatz[1] = 345.7;
 $umsatz[2] = 12456.99;  //kein Ende vorgegeben
 
 print"<hr/>";
-print "<h6>Umsatzzahlen:</h6>";
+print "<h5>Umsatzzahlen:</h5>";
 for($i = 0; $i < 3; $i++)
 {
     print $umsatz[$i]."<br/>";
@@ -43,10 +67,31 @@ $kunde["name"] = "Heinz";
 $kunde["Umsatz"] = 999.99;
 
 print "<hr/>";
-print "<h6>Der Kunde</h6>";
+print "<h5>Der Kunde</h5>";
 foreach($kunde as $key => $value)   // Achtung: NEU
 {
     print "$key: $value<br/>";
 }
 
+// alternative Initialisierung eines Arrays
+$artikel = array("anr" => 123, "bezeichnung" => "hose", "preis" => 9.99);
+
+print "<hr/>";
+print "<h5>Der Artikel</h5>";
+// Testausgabe des Arrays
+print_r($artikel);
+
+// ein zweidimensionales Array
+$ticketpool[0] = array("tnr" => "T1", "Band" => "Band 1");
+$ticketpool[1] = array("tnr" => "T2", "Band" => "Band 2");
+
+print "<hr/>";
+print "<h5>Die Tickets</h5>";
+foreach($ticketpool as $ticket)
+{
+    foreach($ticket as $key => $value)
+    {
+        print "$key: $value<br/>";
+    }
+}
 ?>
