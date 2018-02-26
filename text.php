1f9a1667edbc5f7a9f10b664565ff54c284f0a31
vzim <veronique@programmiertier.de>
2018-02-26 14:58:21 +0100

    Initial commit

---
+++ text.php
@@ -0,0 +1,26 @@
+<?php
+// Variablen
+// aber ohne Datentyp
+
+$zahl = 41;
+$zahl = "Hallo";
+
+$zahl1 = 3.14;
+$zahl2 = 4;
+
+$text1 = "Hallo";
+$text2 = "Welt";
+
+
+// Ausgabe
+// echo $zahl;
+print $zahl;
+
+// Berechnung in Ausgaben
+print "Ergebnis = " .($zahl1 * $zahl2)."<br>";
+print "Erste Zahl ist $zahl1 <br>";
+print "Zweite Zahl ist $zahl2 <br>";
+
+// Verbinden von zwei Texten
+print "DER Satz in der Programmierung = " .$text1.$text2."<br>";
+?>
