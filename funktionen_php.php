cda00da52ce6fcd5a92794afb371d179ad5dd6c5
VeroniqueZimmermann <zimmermannveronique@protonmail.com>
2018-02-27 08:39:29 +0100

    funktionen in php

---
+++ funktionen_php.php
@@ -0,0 +1,37 @@
+<?php
+
+print "<h1>Uebesrchrift</h1>";
+
+print "<font size='7'>Andere Schriftgroesse</font>";
+
+print '<font size="7">Andere Schriftgroesse</font>';
+
+print "<p align='right'>Hier ist ein Absatz";
+print "<b>mit was Wichtigem</b>";
+print "<br/>";
+print "</p>";
+
+print "
+    <p align='justify'>Hier ist ein Basatz
+    <br/>>
+    <h4>Kapitelueberschrift</h4>
+    <hr/>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    <br/>
+    <hr/>
+    </p>
+    ";
+?>
+
+<hr/>
+<ul>
+    <li>Element 1</li>
+    <li>Element 2</li>
+    <li>Element 3</li>
+</ul>
+
+<?php
+
+print "<br/>Und jetzt noch etwas php";
+
+?>
